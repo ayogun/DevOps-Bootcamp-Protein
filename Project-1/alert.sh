f4b418b821a84cef19e17abf8bb2eff15bf40d5d
A.YOGUN <yogun@student.42heilbronn.de>
2022-05-29 15:08:35 +0200

    Update alert.sh

--- Project-1/alert.sh
+++ Project-1/alert.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
+###########################################################################
+# Author  : Ali Yigit Ogun                                                |      
+# Date    : 26.05.2022                                                    |
+# Version : 0.1                                                           |
+# Usage   : This script alerts the specified emmail owner in case of      |
+#	    any of partitions or dısks are occupıed more than %90.        |
+###########################################################################
 
-LIMIT=10
-DISKS=$(df | grep '^/dev/sd' | awk -F' '  'int($5)>(0){printf "%s %d \n", $1, $5}')
+#Variables are declared
+#Limit has been set to 90 arbitrarily.
+LIMIT=90
+
+#Disks and partitions with their use as percentage are displayed.
+#We grep only ones which includes /dev/sd in order to grep disks and partitions.
+DISKS=$(df | grep '^/dev/sd' | awk -F' '  'int($5)>(90){printf "%s %d \n", $1, $5}')
+
+#Number of arguments are counted in order to use it in for loop.
 ARG=$(echo $DISKS | wc -w)
+
+#Hostname and date are collected in order to use in email content.
 HOST=$(hostname)
 NOW=$(date)
-SUBJECT="$HOSTNAM Disk Usage Alert: x used DISK USAGE ALERT: PERCENTused"
-	#USED_AMOUNT=$line| awk '{print $5}' | cut -d% -f1
-	#USED_DISK=$line | awk '{print $1}'
+	
+	#Looping through arguments to send e-mail seperately for each disks and partitions.
 	for((i=1 ; i <=$ARG ; i++)) do
 	PARTITION=$(echo $DISKS | cut -d" " -f $i)
 	((i=i+1))
 	USE=$(echo $DISKS | cut -d" " -f $i)
 	SUBJECT="$HOST Disk Usage Alert: $USE% used"
-	mailx -s "$SUBJECT" yigitogun@gmail.com << EOF
+	mail -s "$SUBJECT" yigitogun@gmail.com << EOF
 			The partition $PARTITION on $HOST has used: $USE% at $NOW
 EOF
 done
